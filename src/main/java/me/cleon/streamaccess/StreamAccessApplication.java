d2864e8e916e2a7225e138872fae81d95a5e2267
caribbeanyute <smithcleon@gmail.com>
2022-02-25 10:00:23 -0500

    initial commit

---
+++ src/main/java/me/cleon/streamaccess/StreamAccessApplication.java
@@ -0,0 +1,13 @@
+package me.cleon.streamaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamAccessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/cleon/streamaccess/StreamAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cleon.streamaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
